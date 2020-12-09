88455d45969ac7838a51a209039a7b67ee091300
lmizinski <ukaszm@gmail.com>
2020-12-09 18:16:27 +0100

    Update laravel_database.sh

--- laravel_database.sh
+++ laravel_database.sh
@@ -4,6 +4,7 @@ source "./configs/config.ini"
 ######## operacje na bazie danych - dopiero po ręcznej aktualizacji configu ###################
 if $laravelVoyager -eq 1; 
 then
+  echo "Running laravelVoyager";
   sudo docker exec -ti php php $dockerArtisanPath voyager:admin your@email.com --create
   sudo docker exec -ti php php $dockerArtisanPath migrate
   sudo docker exec -ti php php $dockerArtisanPath db:seed --class=VoyagerDatabaseSeeder
