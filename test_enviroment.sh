6d8d86364995d95f0630704a970cf149414208a6
lmizinski <ukaszm@gmail.com>
2020-12-09 12:12:55 +0100

    ssh folder and check

--- test_enviroment.sh
+++ test_enviroment.sh
@@ -17,20 +17,31 @@ docker-compose -v
 printf '%b' "${const_TextYellow}" "Sprawdzenie poprawności zainstalowania aplikacji: git:  " "${const_TextPlain}" '\n'
 git --version
 
+printf '%b' '\n'
+printf '%b' "${const_TextRed}" "If above list contains [command not found] message - you should install software accordingly  " "${const_TextPlain}" '\n'
+printf '%b' '\n'
+
 # test whether config file was created
 username=`whoami`
-FILE_PATH="/home/${username}/app/scripts/configs/config.ini"
-
-FILE=configs/config.ini
-if [ -f "$FILE" ] || [ -f "$FILE_PATH" ]; then
-	printf '%b' "${const_TextGreen}" "config file ($FILE) exists. " "${const_TextPlain}" '\n'
+CONFIG_FILE_ABSOLUTE_PATH="/home/${username}/app/scripts/configs/config.ini"
+CONFIG_FILE_RELATIVE_PATH=configs/config.ini
+if [ -f "$CONFIG_FILE_ABSOLUTE_PATH" ] || [ -f "$CONFIG_FILE_RELATIVE_PATH" ]; then
+	printf '%b' "${const_TextGreen}" "config file ($CONFIG_FILE_ABSOLUTE_PATH) exists. " "${const_TextPlain}" '\n'
 else
-	printf '%b' "${const_TextRed}" "config file ($FILE) does not exist. " "${const_TextPlain}" '\n'
+	printf '%b' "${const_TextRed}" "config file ($CONFIG_FILE_ABSOLUTE_PATH) does not exist. Run bash copy_config.sh to fix it. Remember to update config." "${const_TextPlain}" '\n'
+fi
+
+# test whether ssh file was generated
+SSH_FILE_ABSOLUTE_PATH="/home/${username}/app/scripts/ssh/id_rsa.pub"
+SSH_FILE_RELATIVE_PATH=configs/config.ini
+if [ -f "$SSH_FILE_ABSOLUTE_PATH" ] || [ -f "$SSH_FILE_RELATIVE_PATH" ]; then
+	printf '%b' "${const_TextGreen}" "ssh file ($SSH_FILE_ABSOLUTE_PATH) exists. " "${const_TextPlain}" '\n'
+else
+	printf '%b' "${const_TextRed}" "ssh file ($SSH_FILE_ABSOLUTE_PATH) does not exist. Run bash generate_ssh.sh to fix it. Copy this file to github repository. " "${const_TextPlain}" '\n'
 fi
 
 
 printf '%b' '\n'
-printf '%b' "${const_TextRed}" "If above list contains [command not found] message - you should install software accordingly  " "${const_TextPlain}" '\n'
 printf '%b' '\n'
 printf '%b' "${const_TextYellow}" "Press [enter] to exit " "${const_TextPlain}" '\n'
 read clickAnythingToExit
