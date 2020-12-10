47649ee45b3660436bc6940786ae523f1e16436b
lmizinski <ukaszm@gmail.com>
2020-12-10 15:38:39 +0100

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 source "./configs/config.ini"
+
+const_TextPlain='\e[0m'  
+const_TextYellow='\e[1;33m' 
+
 if [[ "$postgresql" -eq 1 ]]; then
-  echo "Creating $postgresqlPath"
+  printf '%b' "${const_TextYellow}" "Creating $postgresqlPath" "${const_TextPlain}" '\n'
   mkdir "$postgresqlPath"
 fi
