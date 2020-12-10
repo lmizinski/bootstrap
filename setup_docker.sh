b98b1efd029d80b631f1e551507f83d50c51f140
lmizinski <ukaszm@gmail.com>
2020-12-10 17:04:03 +0100

    Update setup_docker.sh

--- setup_docker.sh
+++ setup_docker.sh
@@ -6,6 +6,14 @@ const_TextYellow='\e[1;33m'
 
 cd $wwwPath
 
+# test whether ssh file was generated
+NGINX_CONFIG_FILE_ABSOLUTE_PATH="/home/lmi/code/www/configs/nginx/default.conf"
+if [ -f "$NGINX_CONFIG_FILE_ABSOLUTE_PATH" ]; then
+	printf '%b' "${const_TextGreen}" "ssh file ($NGINX_CONFIG_FILE_ABSOLUTE_PATH) exists. " "${const_TextPlain}" '\n'
+else
+	printf '%b' "${const_TextRed}" "ssh file ($NGINX_CONFIG_FILE_ABSOLUTE_PATH) does not exist. Run bash generate_ssh.sh to fix it. Copy this file to your github repository. " "${const_TextPlain}" '\n'
+fi
+
 printf '%b' "${const_TextYellow}" "Postawienie kontenerów dockera z pliku docker-compose.yml" "${const_TextPlain}" '\n'
 sudo docker-compose up -d --build
 printf '%b' "${const_TextYellow}" "Sprawdzenie kontenerów (wszystkie kontenery powinne mieć status up) " "${const_TextPlain}" '\n'
