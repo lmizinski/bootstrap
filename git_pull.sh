fa93e43fdb49117c8543f258b9d4aeb2d6741dd3
lmizinski <ukaszm@gmail.com>
2020-12-11 05:57:34 +0100

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -9,4 +9,12 @@ cd $wwwPath
 ######## start usług ###########################
 sudo docker exec -d php php-fpm
 
+(cd /home/lmi/app/localvolume/www; sudo docker-compose stop nginx)
+(cd /home/lmi/app/localvolume/www; sudo docker-compose start nginx) 
+(cd /home/lmi/app/localvolume/www; sudo docker exec -ti nginx service nginx start)
+
+(cd /home/lmi/app/localvolume/www; sudo docker-compose stop php)
+(cd /home/lmi/app/localvolume/www; sudo docker-compose start php)
+(cd /home/lmi/app/localvolume/www; sudo docker exec -d php php-fpm)
+
 echo "Kod podegrany"
