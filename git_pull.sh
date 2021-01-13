53296ee7dae27682c3ed1c1cb1999ddcc776b27a
lmizinski <ukaszm@gmail.com>
2021-01-13 14:41:22 +0100

    colors

--- git_pull.sh
+++ git_pull.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source "./configs/config.ini"
+source "functions.sh"
 
 cd $wwwPath
 sshCommand='ssh-add ../../scripts/ssh/id_rsa; git reset --hard; git pull '"'${gitAccess}'"
@@ -9,14 +10,17 @@ cd $wwwPath
 ######## start usług ###########################
 sudo docker exec -d php php-fpm
 
+print_message "Restarting services:  nginx" 'yellow'
 (cd $wwwPath; sudo docker-compose stop nginx)
 (cd $wwwPath; sudo docker-compose start nginx) 
 (cd $wwwPath; sudo docker exec -ti nginx service nginx start)
 
+print_message "Restarting services:  php" 'yellow'
 (cd $wwwPath; sudo docker-compose stop php)
 (cd $wwwPath; sudo docker-compose start php)
 (cd $wwwPath; sudo docker exec -d php php-fpm)
 
+print_message "Restarting services:  postgres" 'yellow'
 (cd $wwwPath; sudo docker-compose stop postgres)
 (cd $wwwPath; sudo docker-compose start postgres)
 
