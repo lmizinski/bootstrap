9a88c84dd4c784835b5bfbfa59c1ce2bb244b7f6
lmizinski <ukaszm@gmail.com>
2021-01-13 14:08:07 +0100

    easier debug

--- update.sh
+++ update.sh
@@ -5,8 +5,11 @@ cd $wwwPath
 sshCommand='ssh-add ../../scripts/ssh/id_rsa; git reset --hard; git pull '"'${gitAccess}'"
 ssh-agent bash -c "$sshCommand"
 
+echo "Running: composer install"
 sudo docker exec -ti composer composer install
+echo "Running: artisan migrate"
 sudo docker exec -ti php php "$dockerArtisanPath" migrate
+echo "Running: artisan migrate:status"
 sudo docker exec -ti php php "$dockerArtisanPath" migrate:status
 
 cd $scriptsPath
