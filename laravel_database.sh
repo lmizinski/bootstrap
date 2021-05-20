d93188e39bd58ed2064cca09444dd36dff3afc5e
lmizinski <ukaszm@gmail.com>
2021-05-20 11:49:19 +0200

    Update laravel_database.sh

--- laravel_database.sh
+++ laravel_database.sh
@@ -7,6 +7,7 @@ if [[ "$laravelVoyager" -eq 1 ]]; then
   sudo docker exec -ti php php $dockerArtisanPath voyager:admin your@email.com --create
   sudo docker exec -ti php php $dockerArtisanPath migrate
   sudo docker exec -ti php php $dockerArtisanPath db:seed --class=VoyagerDatabaseSeeder
+  sudo docker exec -ti php php $dockerArtisanPath create:roles
 fi
 if [[ "$passport" -eq 1 ]]; then
   echo "Running passport:install";
