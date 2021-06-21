438836a39a2558857d797949c1b0c309beb85b95
lmizinski <ukaszm@gmail.com>
2021-06-21 15:47:22 +0200

    Create git_reset_hard_origin_master.sh

---
+++ git_reset_hard_origin_master.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source "./configs/config.ini"
+source "functions.sh"
+
+print_message "git pull" 'yellow'
+cd $wwwPath
+sshCommand='ssh-add ../../scripts/ssh/id_rsa;  git fetch origin; git reset --hard origin/master;'
+ssh-agent bash -c "$sshCommand"
+
+cd $scriptsPath
+bash "$scriptsPath/restart_services.sh"
+
+print_message "Code updated" 'green'
